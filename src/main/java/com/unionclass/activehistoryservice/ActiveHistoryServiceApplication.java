c9a61555ebcb9e8335381acc7d0731e34914aeaf
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-06-10 12:19:56 +0900

    chores: default settings

---
+++ src/main/java/com/unionclass/activehistoryservice/ActiveHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unionclass.activehistoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unionclass/activehistoryservice/ActiveHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unionclass.activehistoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
