9b222dce6a3f78f8656c98f74c45e78690b12012
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-06-24 12:04:28 +0900

    refactor: ReviewCreatedEvent refactoring

--- src/main/java/com/unionclass/activehistoryservice/common/kafka/entity/event/ReviewCreatedEvent.java
+++ src/main/java/com/unionclass/activehistoryservice/common/kafka/entity/event/ReviewCreatedEvent.java
@@ -13,16 +13,18 @@ public class ReviewCreatedEvent {
     private String reviewerUuid;
     private String revieweeUuid;
     private String reviewId;
+    private Double rating;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     @Builder
     public ReviewCreatedEvent(
             String reviewerUuid, String revieweeUuid, String reviewId,
-            LocalDateTime createdAt, LocalDateTime updatedAt) {
+            Double rating, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.reviewerUuid = reviewerUuid;
         this.revieweeUuid = revieweeUuid;
         this.reviewId = reviewId;
+        this.rating = rating;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
